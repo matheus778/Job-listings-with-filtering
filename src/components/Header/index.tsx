03faac105d7b7cdb52dc33ca76105217e2127dde
Matheus Vinicius <57428641+matheus778@users.noreply.github.com>
2023-01-26 16:52:24 -0300

    ajustes de responsividade

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,6 +2,7 @@ import { useContext } from 'react';
 import { FilterContext } from '../../context/context';
 
 import bgHeaderDesktop from '../../assets/bg-header-desktop.svg';
+import bgHeaderMobile from '../../assets/bg-header-mobile.svg';
 import iconRemove from '../../assets/icon-remove.svg'
 import './style.css';
 
@@ -22,12 +23,14 @@ export function Header() {
   return (
     <>
       <header className="header">
-        <img src={bgHeaderDesktop} alt="imagem de fundo" />
+        {/* <img src={ bgHeaderMobile} alt="imagem de fundo" /> */}
+        <div className="bg-image"></div>
       </header>
 
       {state.name.length > 0 && (
         <div className="filter">
         <div className="filter-content">
+
           {state.name.length > 0 && state.name.map((filter, index) => {
             return (
               <div key={`${filter}${index}`} className="filter-selected">
